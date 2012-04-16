2ad5a605b620d3aa67ed92e4b1a1a942c29637f1
Drew Waddell <dwaddell@orchardsoft.com>
2012-04-16 10:20:21 -0400

    Added

--- Source Code/Visual C++ Project/Utilities.h
+++
@@ -1,9 +0,0 @@
-#include "4DPluginAPI.h"
-#include "4DPlugin.h"
-
-#include "PrivateTypes.h"
-#include "EntryPoints.h"
-
-char * getTextParameter(PA_PluginParameters, short);
-void freeTextParameter(char *);
-void setError (long value); // MJG 7/5/05 #8372
